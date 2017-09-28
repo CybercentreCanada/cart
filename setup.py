88591fc9ce6aa3f35f8149b3db3ea73de8ba28d7
sgaron-cse <sgaron.cse@gmail.com>
2017-09-28 11:24:36 -0400

    Cleaned up setup.py for pypi distribution

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 """CaRT PiP Installer"""
 
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 from cart import cart
 
@@ -10,18 +9,13 @@ setup(
                           cart.__build_minor__, 
                           cart.__build_micro__),
     description='CaRT Neutering format',
-    long_description="Compressed and RC4 Transport (CaRT) Neutering format.",
+    long_description="Compressed and RC4 Transport (CaRT) Neutering format. This is a file format that is used to "
+                     "neuter malware files for distribution in the malware analyst community.",
     url='https://github.com/CommunicationsSecurityEstablishment/cart',
-    author='sgaron',
-    author_email='sgaron.cse@gmail.com',
+    author='CSE-CST Assemblyline development team',
+    author_email='assemblyline-cse-cst@googlegroups.com',
     license='MIT',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
@@ -34,12 +28,9 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
-    keywords='cart',
+    keywords='neutering format malware cart stix development gc canada cse-cst cse cst',
     packages=find_packages(exclude=['docs', 'unittest']),
     install_requires=['pycrypto'],
-    extras_require={},
-    package_data={},
-    data_files=[],
     entry_points={
         'console_scripts': [
             'cart=cart:main',
